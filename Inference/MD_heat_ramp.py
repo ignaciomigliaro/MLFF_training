dfab59c4c6dc78f75cad37688fed0efbeeaca24e
Ignacio Migliaro <ignaciomigliaro@outlook.com>
2025-01-13 16:33:09 -0600

    Fixed errors,

--- Inference/MD_heat_ramp.py
+++ Inference/MD_heat_ramp.py
@@ -1,3 +1,5 @@
+import os
+os.environ["PYTHONWARNINGS"] = "ignore::FutureWarning"
 from ase.io import read, write
 from ase import units
 from ase.md.langevin import Langevin
@@ -14,14 +16,19 @@ from mace.calculators import MACECalculator
 import matplotlib.pyplot as plt
 from ase.md.npt import NPT
 from ase.md.nvtberendsen import NVTBerendsen
-        
-        
+import warnings
+import logging
+from pathlib import Path
+logging.getLogger("torch").setLevel(logging.ERROR)
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+import sys  
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Python code to run Langevin MD and use a heat reamp")
     parser.add_argument("input_file",type=str,help="Coordinate file that contains the initial configuration")
-    parser.add_argument("output_file",type=str,require=True,help='XYZ file path and name to output properties')
+    parser.add_argument("output_file",type=str,help='XYZ file path and name to output properties')
     parser.add_argument("--start_temp",type=float,required=True,help='Starting temperature for the heat ramp')
     parser.add_argument("--final_temp",type=float,required=True,help='Final temperature for the heat ramp')
     parser.add_argument("--temp_steps",type=int,required=True,help="Number of steps needed to final temperature")
@@ -32,9 +39,9 @@ def parse_arguments():
     parser.add_argument("--ensemble",type=str,default="npt",choices=['nve,nvt,npt'],help="Ensemble used for ensamble run (NVE | NPT | NVE)")
     return parser.parse_args()
 
-def NPT_calc(init_conf, temp, calc, fname, s, T):
+def NPT_calc(init_conf, temp, calc, fname, s, T,timestep):
     init_conf.calc = calc
-    dyn = NPT(init_conf, 1.0*units.fs, temperature_K=temp, trajectory='npt_600k_n50_T10ps.traj',logfile='npt.log',externalstress=1.0*units.bar,ttime=20*units.fs,pfactor=2e6*units.fs**2,append_trajectory=True)
+    dyn = NPT(init_conf, timestep*units.fs, temperature_K=temp, trajectory='npt_600k_n50_T10ps.traj',logfile='npt.log',externalstress=1.0*units.bar,ttime=20*units.fs,pfactor=2e6*units.fs**2,append_trajectory=True)
 
     def write_frame(temp):
             dyn.atoms.info['energy_mace'] = dyn.atoms.get_potential_energy()
@@ -71,19 +78,22 @@ def ramp_temperature(start_temp,end_temp,step,n_steps):
         return temp
 
 def main():
+    print('starting')
     args = parse_arguments()
     start_temp = args.start_temp  # Initial temperature in K
     end_temp = args.final_temp   # Final temperature in K
-    n_steps = args.nsteps   # Total number of MD steps
-    device = args.device
+    n_steps = args.nstep   # Total number of MD steps
+    device = str(args.device)
+    timestep = args.timestep
     temp_steps = args.temp_steps
     input_file = args.input_file
     output_file = args.output_file
+    model_path = None
+    print(f"Starting temperature at {start_temp}K ending in {end_temp}K in {temp_steps}. MD simualtion is run at {timestep} in {n_steps}")
     #if not model path is given use the MACE-off as default
-    if args.model_path:
-        calc = MACECalculator(args.model_path,device=device)
-    else:
-        calc = mace_off(model="medium", device=args.device)
+    
+    calc = mace_off(model="medium", device=device)
+    print('Using MACE-off')
 
     #Read initial configuration
     init_conf = read(input_file)
@@ -94,17 +104,16 @@ def main():
     MaxwellBoltzmannDistribution(current_conf, temperature_K=start_temp) #Initialize temperature at starting temp
     Stationary(current_conf)
     ZeroRotation(current_conf)
-
+    print('working')
     for step in range(temp_steps):
         # Update the target temperature for the ramp
         current_target_temp = ramp_temperature(start_temp,end_temp,step,temp_steps)
         fname = output_file
-        print(f"Running MD calculation with {t}K")
+        print(f"Running MD calculation with {current_target_temp}K")
         
         # Run the MD simulation
-        NPT_calc(current_conf, temp=current_target_temp, calc=calc, fname=fname, s=1, T=n_steps)
+        NPT_calc(current_conf, temp=current_target_temp, calc=calc, fname=fname, s=1, T=n_steps,timestep=timestep)
 
 
 if __name__ == '__main__':
     main()
-        
